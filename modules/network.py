caeb9a2c4442adf37b4ea575d14cb3f84c2308ba
sxrabyte <sxrabyte@proton.me>
2026-06-04 12:57:21 +0200

    ~ Added Network Module

---
+++ modules/network.py
@@ -0,0 +1,71 @@
+import subprocess
+from dataclasses import dataclass
+
+from pygments import highlight
+
+
+@dataclass
+class Finding:
+    label: str
+    output: str
+    highlight: bool
+
+#To run system commands using python
+def run_command(cmd: str) -> str:
+    try:
+        result = subprocess.run(
+            cmd, shell=True, capture_output=True, text=True
+        )
+        return result.stdout.strip() or result.stderr.strip()
+    except Exception as e:
+        return f"Error: {e}"
+
+
+
+def enumerate_pe() -> list[Finding]:
+    findings = []
+
+    findings.append(Finding(
+        label = "Processes",
+        output = "ps aux",
+        highlight = False,
+    ))
+
+    findings.append(Finding(
+        label = "Process tree with parents",
+        output = "ps -ef --forest",
+        highlight = False
+    ))
+
+    findings.append(Finding(
+        label = "Listening TCP Ports",
+        output = "ss -tlnp",
+        highlight = True
+    ))
+
+    findings.append(Finding(
+        label = "Listening UDP Ports",
+        output = "ss -ulnp",
+        highlight = False
+    ))
+
+    findings.append(Finding(
+        label = "Network Interfaces & IPs",
+        output = "ip a / ifconfig",
+        highlight = False
+    ))
+
+    findings.append(Finding(
+        label = "local hostname mappings",
+        output = "cat /etc/hosts",
+        highlight = False
+    ))
+
+    findings.append(Finding(
+        label = "ARP table",
+        output = "arp -a",
+        highlight = False
+    ))
+
+
+    return findings
